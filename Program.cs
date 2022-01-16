fe474de2cce35fd9727e46d9ed2ed6aeadbf7db1
yppyrz <yppyrz@gmail.com>
2022-01-16 17:13:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dizi tanımlama
+            string[] renkler = new string[5]; // 5 elemanlı string dizisi.
+
+            string[] hayvanlar = { "Kedi", "Köpek", "Kuş", "Maymun" };
+
+            int[] dizi; // elemanı belli değilse, sonradan atama yapmak istersek.
+            dizi = new int[5]; // sonradan eleman sayısı atadık.
+
+            // Dizilere değer atama ve erişim
+            renkler[0] = "Mavi";
+            dizi[3] = 10;
+
+            Console.WriteLine(hayvanlar[0]);// Diziler 0. indexten başlar
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            // Döngülerle dizi kullanımı
+            // Klavyeden girilen n tane sayının ortalamasını alan program.
+            Console.WriteLine("Dizinin eleman sayısını giriniz:");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.WriteLine("Lütfen {0}. sayıyı giriniz",i+1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+            int toplam = 0;
+            foreach (var sayi in sayiDizisi)
+            {
+                toplam += sayi;
+            }
+            Console.WriteLine("Ortalaama :" + toplam/diziUzunlugu);
+
+        }
+    }
+}
